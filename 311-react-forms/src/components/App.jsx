8a615bdd116b3f9105fe882d24b326bb821ceb8a
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-05-10 12:07:52 +1000

    feat: implement form input handling and dynamic greeting in App component

--- 311-react-forms/src/components/App.jsx
+++ 311-react-forms/src/components/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [formInput, setFormInput] = useState("");
+  const [submitStatus, setSubmitStatus] = useState(false);
+  const [heading, setHeading] = useState("");
+  const handleChange = (e) => {
+    setFormInput(e.target.value);
+
+  };
+  const handleSubmit = () => {
+    // setSubmitStatus(true);
+    setHeading(formInput);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {heading}</h1>
+      <input type="text" onChange={handleChange} placeholder="What's your name?" value={formInput}/>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
