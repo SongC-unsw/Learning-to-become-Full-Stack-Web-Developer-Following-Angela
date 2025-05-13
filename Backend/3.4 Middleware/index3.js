9bfd13c1ae5d98c86ca6048122dbc286c9e95015
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-05-13 22:50:34 +1000

    feat: add custom logger middleware to log request method and URL

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const logger = (req, res, next) => {
+  console.log(`Request Method: ${req.method}\nRequest URL: ${req.url}`);
+  next();
+};
 
 app.use(logger);
 
