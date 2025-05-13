5510769e17c22db5d0c6a53021f4248970d2301e
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-05-13 13:29:30 +1000

    feat: add QR code generation functionality with user input and save URL to file

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -3,3 +3,39 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from "inquirer";
+import isUrl from "is-url";
+import fs from "fs";
+import qr from "qr-image";
+const options = {type: 'png'};
+const questions = [
+    {
+        type: 'input',
+        name: 'URL',
+        message: 'Please enter a URL that you want to turn into QR code',
+        default: 'https://google.com',
+        validate: function(value) {
+            if (isUrl(value)) {
+                return true;
+            } else {
+                return "Please enter a valid URL";
+            }
+        }
+    }
+]
+inquirer.prompt(questions)
+.then((answers) => {
+    fs.writeFile("./message.txt", answers.URL, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log("URL saved to message.txt");
+        }
+    });
+    const qr_png = qr.image(answers.URL,options);
+    qr_png.pipe(fs.createWriteStream('my_qrcode.png'));
+    console.log("QR created");
+})
+.catch((err) => {
+    console.log(err);
+})
